9bccfd3bf3e4c15a767239060f7e212c3e88b6ad
HatemNofal <h.nofal@unicomg.com>
2020-02-11 18:27:44 +0200

    ce

---
+++ dockerapi/Controllers/ValuesController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace dockerapi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> GetValues()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        //[HttpGet]
+        //public string Value(int id)
+        //{
+        //    return "value";
+        //}
+
+        [HttpPost]
+        public void SaveNewValue([FromBody]string value)
+        {
+        }
+
+        [HttpPut]
+        public void UpdateValue(int id, [FromBody]string value)
+        {
+        }
+
+        [HttpDelete]
+        public void RemoveValue(int id)
+        {
+        }
+    }
+}
--- dockerapi/Startup.cs
+++ dockerapi/Startup.cs
@@ -1,15 +1,13 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Swagger;
+
 
 namespace dockerapi
 {
@@ -26,6 +24,10 @@ namespace dockerapi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Swagger Movies Demo", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,7 +39,13 @@ namespace dockerapi
             }
 
             app.UseHttpsRedirection();
+            app.UseSwagger();
 
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Swagger Movies Demo V1");
+            });
             app.UseRouting();
 
             app.UseAuthorization();
